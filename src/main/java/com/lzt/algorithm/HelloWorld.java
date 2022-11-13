c0bd1c38e18c0fa2d83104cb5171cbb07fcf84ef
lzt <woshilizeteng@gmail.com>
2022-11-13 19:04:35 +0800

    Init.

---
+++ src/main/java/com/lzt/algorithm/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lzt.algorithm;
+
+/**
+ * @author lzt
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
