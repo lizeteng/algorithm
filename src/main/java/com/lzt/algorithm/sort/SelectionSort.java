550fecfda28a2dcbe12feb79302397189cb7c3bc
lzt <woshilizeteng@gmail.com>
2022-11-13 20:24:17 +0800

    Add selection sort.

---
+++ src/main/java/com/lzt/algorithm/sort/SelectionSort.java
@@ -0,0 +1,35 @@
+package com.lzt.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author lzt
+ */
+public class SelectionSort {
+
+    public static int[] sort(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return nums;
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[min] > nums[j]) {
+                    min = j;
+                }
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[min];
+            nums[min] = temp;
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(sort(nums)));
+    }
+}
