371e00fe80491106a55a452315f6e98bc6801535
lzt <woshilizeteng@gmail.com>
2022-11-13 20:31:21 +0800

    Format code.

--- src/main/java/com/lzt/algorithm/sort/BubbleSort.java
+++ src/main/java/com/lzt/algorithm/sort/BubbleSort.java
@@ -20,6 +20,7 @@ public class BubbleSort {
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
+                    
                     flag = true;
                 }
             }
