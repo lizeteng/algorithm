d5ec6be2f9c74632604b041b6dd27d16facb070a
lzt <woshilizeteng@gmail.com>
2022-11-16 00:35:47 +0800

    Add binary search first priority.

---
+++ src/main/java/com/lzt/algorithm/binarysearch/BinarySearchFirstPriority.java
@@ -0,0 +1,36 @@
+package com.lzt.algorithm.binarysearch;
+
+/**
+ * @author lzt
+ */
+public class BinarySearchFirstPriority {
+
+    public static int search(int[] nums, int target) {
+        if (nums == null || nums.length < 1) {
+            return -1;
+        }
+
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >>> 1);
+
+            if (nums[mid] == target && (mid == 0 || nums[mid - 1] != target)) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+
+                continue;
+            }
+
+            high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[] {1, 1, 1, 1, 1, 6, 7, 8, 9}, 1));
+    }
+}
